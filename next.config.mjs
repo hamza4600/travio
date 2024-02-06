a8581c575c5d8c73378aba7ae9b424b7f8bebea4
hamza4600 <hkhantanoli@gmail.com>
2024-02-06 18:16:04 +0500

    second commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: { remotePatterns: [{ hostname: 'cdn.sanity.io' }] },
-    
+    swcMinify: true,
+    compiler: {
+      styledComponents: true,
+    },
 };
 
 export default nextConfig;
