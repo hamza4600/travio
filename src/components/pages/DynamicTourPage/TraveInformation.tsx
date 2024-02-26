0913d952c72a8d59dd89ae8c65b901a19ab785d4
Hamza Ali <mastank387@gmail.com>
2024-02-26 21:26:43 +0500

    fix build

--- src/components/pages/DynamicTourPage/TraveInformation.tsx
+++ src/components/pages/DynamicTourPage/TraveInformation.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import Image from "next/image";
-
 // import { localizedString, PropsWithLocale } from "@/contexts/LocaleProvider";
 // import { urlFor } from "@/sanity/client";
 // import { SanityTravelInfoSection } from "@/sanity/types";
