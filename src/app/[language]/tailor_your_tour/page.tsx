bdac75e6c9fcaac552a9f09f1e0b25f664573a0b
hamza4600 <hkhantanoli@gmail.com>
2024-03-08 21:12:52 +0500

    added Tailor page missing data

--- src/app/[language]/tailor_your_tour/page.tsx
+++ src/app/[language]/tailor_your_tour/page.tsx
@@ -1,9 +1,50 @@
-import { getTailorPage } from "@/lib/sanity.TailorPage";
+import { getTailorPage, getTailorPageSeo } from "@/lib/sanity.TailorPage";
 import dynamic from "next/dynamic";
+import { urlForImage } from "../../../../sanity/lib/image";
 const TailorYourTourPage = dynamic(() => import("@/components/pages/TrailYourTour"));
 
+export async function generateMetadata({ params }) {
+  const { language } = params;
+
+  const seo = await getTailorPageSeo();
+  const meta = seo?.meta_data || {};
+  const metaTitle = meta?.meta_title[language];
+  const metaDescription = meta?.meta_description[language];
+  const metaImage = meta?.meta_image;
+  const keywords = meta?.meta_keywords[language];
+  const imgUrl = urlForImage(metaImage?.asset?._ref);
+
+  return {
+    title: metaTitle,
+    description: metaDescription,
+    keywords: keywords,
+    image: imgUrl,
+    language: language,
+    openGraph: {
+      title: metaTitle,
+      description: metaDescription,
+      images: [
+        {
+          url: imgUrl,
+          width: 1200,
+          height: 630,
+          alt: metaTitle,
+        },
+        {
+          url: imgUrl, // Provide a larger image URL here
+          width: 1600, // Adjust the width according to your preference
+          height: 900, // Adjust the height according to your preference
+          alt: metaTitle,
+        },
+      ],
+      type: "website",
+      locale: language,
+    },
+  };
+}
+
 // revalidate: 1 hour
-export const revalidate = 3600 
+export const revalidate = 3600
 
 export default async function Index({ params }) {
 
--- src/lib/sanity.TailorPage.tsx
+++ src/lib/sanity.TailorPage.tsx
@@ -1,6 +1,6 @@
 import { pageLayout } from './sanity.HomePage';
 import { CLIENT } from './sanity.const';
-  
+
 const trailorPageQuery = `*[_type == "tailor_your_tour"]`;
 
 const query = `{
@@ -10,4 +10,12 @@ const query = `{
 
 export async function getTailorPage() {
     return await CLIENT.fetch(query)
+}
+
+const seoQuery = `*[_type == "tailor_your_tour"][0]{
+    meta_data
+  }`
+
+export async function getTailorPageSeo() {
+    return await CLIENT.fetch(seoQuery)
 }
