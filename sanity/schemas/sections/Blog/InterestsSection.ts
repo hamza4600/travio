9791bb365536ab2d8ca7ef825c7d7a0e56a3a12a
hamza4600 <hkhantanoli@gmail.com>
2024-04-23 14:33:26 +0500

    xcvgusd gckdhcvd45

--- sanity/schemas/sections/Blog/InterestsSection.ts
+++ sanity/schemas/sections/Blog/InterestsSection.ts
@@ -2,6 +2,45 @@ import { defineArrayMember, defineField, defineType } from 'sanity'
 import {  displayNumber, joinStrings } from '@/lib/utils'
 
 import { Star } from '@phosphor-icons/react'
+// InterestCard
+export const interestCard = defineType({
+  name: 'interest_card',
+  title: 'Interest Card',
+  type: 'object',
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Name',
+      description: 'The name of the interest card',
+      type: 'locale_string',
+    }),
+    defineField({
+      name: 'icon',
+      title: 'Icon',
+      description: 'The icon for the interest',
+      type: 'image',
+    }),
+    defineField({
+      name: 'link',
+      title: 'Link',
+      description: 'The link for the interest',
+      type: 'reference',
+      to: [{ type: 'blog_page' }],
+    }),
+  ],
+  preview: {
+    select: {
+      name: 'name',
+      icon: 'icon',
+    },
+    prepare: ({ name, icon }) => {
+      return {
+        title: name,
+        media: icon,
+      }
+    },
+  },
+})
 
 export default defineType({
   name: 'interests_section',
@@ -22,6 +61,7 @@ export default defineType({
       description: 'The heading for the interests section',
       type: 'locale_string',
     }),
+    //  array of object that will have name , icon , link  of page 
     defineField({
       name: 'interests',
       title: 'Interests',
@@ -29,10 +69,22 @@ export default defineType({
       type: 'array',
       of: [
         defineArrayMember({
-          type: 'reference',
-          to: [{ type: 'tag' }],
+          type: 'interest_card',
         }),
       ],
+      // of: [
+      //   // defineArrayMember({
+      //   //   type: 'reference',
+      //   //   to: [{ type: 'tag' }],
+      //   // }),
+      //   defineArrayMember({
+      //     name: 'interest',
+      //     title: 'Interest',
+      //     description: 'An interest',
+      //     // type: 'interest_card',
+      //     to: [{ type: 'interest_card' }],
+      //   }),
+      // ],
     }),
   ],
   preview: {
