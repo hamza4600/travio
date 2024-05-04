6f74f6a19027200a6307e1f162699f658ec059ea
hamza4600 <hkhantanoli@gmail.com>
2024-05-04 12:42:26 +0500

    d4 d1

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -6,11 +6,9 @@ import {
   SanityGlobals,
 } from "../../../sanity/lib/types";
 
-import { urlFor } from "../../../sanity/lib/client";
 const PromoBanner = dynamic(() => import("../molecules/Promobanner"));
 const Footer = dynamic(() => import("../molecules/footer"));
 const Header = dynamic(() => import("../molecules/Header"));
-import Schema from "../atom/Schema";
 import Breadcrumbs, { Breadcrumb } from "../atom/Breadcrumbs";
 
 const Layout = ({
@@ -32,41 +30,28 @@ const Layout = ({
 }) => {
   return (
     <div className="bg-white">
-      <div>
-        {globals?.navbar?.logo && (
-          <Schema
-            data={{
-              "@context": "https://schema.org",
-              "@type": "Organization",
-              url: process.env.NEXT_PUBLIC_BASE_URL,
-              logo: urlFor(globals?.navbar?.logo),
-            }}
-          />
-        )}
-        <div
-          className="overflow-x-hidden text-black min-h-screen  flex flex-col"
-          style={{
-            width: process.env.NEXT_PUBLIC_DEVELOPMENT ? "" : "",
-          }}
-        >
-          <Header
-            navbar={globals?.navbar}
-            banner={globals?.banner}
-            locale={locale}
-          />
+      <div
+        className="overflow-x-hidden text-black min-h-screen  flex flex-col"
+        style={{
+          width: process.env.NEXT_PUBLIC_DEVELOPMENT ? "" : "",
+        }}
+      >
+        <Header
+          navbar={globals?.navbar}
+          banner={globals?.banner}
+          locale={locale}
+        />
 
-          <PromoBanner banner={promo_banner} locale={locale} />
-          <main
-            className={`grow w-full ${
-              maxWidth ? "max-w-[1440px] mx-auto " : ""
+        <PromoBanner banner={promo_banner} locale={locale} />
+        <main
+          className={`grow w-full ${maxWidth ? "max-w-[1440px] mx-auto " : ""
             }`}
-          >
-            <Breadcrumbs paths={breadcrumbs} locale={locale} />
+        >
+          <Breadcrumbs paths={breadcrumbs} locale={locale} />
 
-            {children}
-          </main>
-          <Footer footer={globals?.footer} language={locale} />
-        </div>
+          {children}
+        </main>
+        <Footer footer={globals?.footer} language={locale} />
       </div>
     </div>
   );
